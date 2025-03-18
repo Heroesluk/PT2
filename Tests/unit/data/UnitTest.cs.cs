8cf65d4a60710ec6988b0a8d649c4f85a2847c09
heroesluk <heroesluk@gmail.com>
2025-03-18 18:42:27 +0100

    Add project files.

---
+++ Tests/unit/data/UnitTest.cs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.unit.data
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
