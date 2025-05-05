f917dd951ced2f410054a7b30f63e200d7c8d9e5
heroesluk <242209@edu.p.lodz.pl>
2025-05-05 21:24:12 +0100

    refactor

---
+++ TestData/Tests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.data
+{
+    [TestClass]
+    public class UnitTest
+    {
+
+        [TestMethod]
+        public void UserConstructor_ShouldSetProperties()
+        {
+            // Given
+            string expectedUsername = "testUser";
+            string expectedPassword = "securePass";
+            string expectedEmail = "test@example.com";
+
+            // When
+            User user = new User(1,expectedUsername, expectedPassword, expectedEmail);
+
+            // Then
+            Assert.AreEqual(expectedUsername, user.Username);
+            Assert.AreEqual(expectedPassword, user.Password);
+            Assert.AreEqual(expectedEmail, user.Email);
+        }
+
+        [TestMethod]
+        public void ToString_ShouldReturnCorrectFormat()
+        {
+            // Given
+            User user = new User(1,"testUser", "securePass", "test@example.com");
+            string expectedOutput = "Id: 1, Username: testUser, Email: test@example.com";
+
+            // When
+            string actualOutput = user.ToString();
+
+            // Then
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+    }
+
+
+
+}
