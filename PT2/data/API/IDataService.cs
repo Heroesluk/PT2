1f290bdc2db18f20fe3cfca405c2397db5d1e03f
Szymon Zajączkowski <250375@edu.p.lodz.pl>
2025-05-05 20:01:39 +0200

    New DataService interface

---
+++ PT2/data/API/IDataService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PT2.data.API
+{
+    public interface IDataService
+    {
+        IUserRepository userRepo { get; }
+        IItemRepository itemRepo { get; }
+        IInventoryStateRepository inventoryStateRepo { get; }
+        IEventRepository eventRepo { get; }
+
+        IDataContext dataContext { get; }
+
+    }
+}
